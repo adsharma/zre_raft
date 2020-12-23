f0801185bc34ac2d7525f099f3ea42be43397889
Arun Sharma <arun@sharma-home.net>
2020-12-23 10:56:29 -0800

    Add signal support
    chat.py -n alice
    chat.py -n bob
    
    Verify that they do a x3dh, compute a shared key and initialize their ratchets

--- zre_raft/chat.py
+++ zre_raft/chat.py
@@ -10,11 +10,16 @@ except Exception as e:
 from aiostream.stream import ziplatest
 from aiostream.aiter_utils import aitercontext
 from collections import defaultdict
+from cryptography.hazmat.primitives.asymmetric.x25519 import (
+    X25519PrivateKey,
+    X25519PublicKey,
+)
 from prompt_toolkit.patch_stdout import patch_stdout
 from prompt_toolkit.shortcuts import PromptSession
 
 import argparse
 import asyncio
+import base64
 import json
 import logging
 import random
@@ -25,6 +30,7 @@ import uuid
 import zmq
 import zmq.asyncio
 import zre_raft
+from zre_signal import b64k, SignalState
 
 
 exit_event = threading.Event()
@@ -34,8 +40,9 @@ class ZRENode:
     GROUP = "raft"
 
     def __init__(self, name):
-        self.n = self.create_node(name)
         self.peers = {}
+        self.directory = {}  # peer by name. Should use consensus to maintain
+        self.create_node(name)
         self.groups = defaultdict(list)
         self.queue = asyncio.Queue()
         self.ctx = zmq.asyncio.Context()
@@ -43,20 +50,61 @@ class ZRENode:
         self.session = PromptSession(f"{self.n.name()} {self.GROUP}: ")
 
         self.threads = []
-        t = threading.Thread(target=self.worker, args=[self.n, self.pipe2, self.queue])
+        t = threading.Thread(target=self.worker, args=[self.pipe2, self.queue])
         self.threads.append(t)
         t.start()
 
-    @staticmethod
-    def create_node(name: str):
-        n = Pyre(name)
+    def create_signal_headers(self):
+        n = self.n
+        n.set_header("IK", b64k(self.signal.IK.public_key()))
+        # TODO: Generate many pre keys and delete the one-time keys after use
+        n.set_header("SPK", b64k(self.signal.SPK.public_key()))
+        n.set_header("OPK", b64k(self.signal.OPK.public_key()))
+
+    def create_node(self, name: str):
+        self.n = n = Pyre(name)
+        self.n.signal = SignalState()
+        # Convenience members self.signal/peer_keys
+        self.signal = self.n.signal
+        self.peer_keys = self.signal.peer_keys
         n.set_header("Version", str(zre_raft.__version__))
+        self.create_signal_headers()
         n.join(ZRENode.GROUP)
         n.start()
         return n
 
-    @staticmethod
-    async def chat_task(n, pipe, queue):
+    async def handle_command(self, command, peer=None):
+        cmd, rest = command.split(" ", maxsplit=1)
+        print("command: ", cmd, rest, peer)
+        if cmd == "/whisper":
+            out = rest.split(" ", maxsplit=1)
+            if len(out) == 2:
+                target, rest = out
+                if target in self.directory:
+                    target = self.directory[target]
+                self.n.whisper(target, rest.encode("utf-8"))
+            else:
+                print("syntax: /whisper peer message")
+        elif cmd == "/encrypt":
+            out = rest.split(" ", maxsplit=1)
+            if len(out) == 2:
+                target, rest = out
+                if target in self.directory:
+                    target = self.directory[target]
+                self.n.signal.establish_session(self.n, target, rest.encode("utf-8"))
+            else:
+                print("syntax: /encrypt peer message")
+        elif cmd == "/ephemeral":
+            if rest:
+                self.peer_keys[peer]["EK"] = X25519PublicKey.from_public_bytes(
+                    base64.b64decode(rest)
+                )
+                self.signal._on_message_receive(peer)
+            else:
+                print("syntax: /ephemeral key")
+
+    async def chat_task(self, pipe, queue):
+        n = self.n
         poller = zmq.Poller()
         poller.register(pipe, zmq.POLLIN)
         poller.register(n.socket(), zmq.POLLIN)
@@ -66,7 +114,12 @@ class ZRENode:
                 continue
             if pipe in items and items[pipe] == zmq.POLLIN:
                 message = await pipe.recv()
-                n.shouts(ZRENode.GROUP, message.decode("utf-8"))
+                message = message.decode("utf-8")
+                if message and message[0] == "/":
+                    # special commands
+                    await self.handle_command(message)
+                else:
+                    n.shouts(ZRENode.GROUP, message)
             else:
                 cmds = n.recv()
                 queue.put_nowait(cmds)
@@ -74,11 +127,11 @@ class ZRENode:
         n.stop()
         queue.put_nowait(None)
 
-    @staticmethod
-    def worker(n, pipe, queue):
+    def worker(self, pipe, queue):
         global worker_loop
+        n = self.n
         worker_loop = loop = asyncio.new_event_loop()
-        task = loop.create_task(ZRENode.chat_task(n, pipe, queue))
+        task = loop.create_task(self.chat_task(pipe, queue))
         loop.run_until_complete(task)
 
     @staticmethod
@@ -118,7 +171,22 @@ class ZRENode:
     async def handle_whisper(self, cmds):
         peer = uuid.UUID(bytes=cmds.pop(0))
         name = cmds.pop(0).decode("utf-8")
-        print(f"{peer}: {cmds}")
+        message = cmds.pop(0).decode("utf-8")
+        if message and message[0] == "/":
+            # special commands
+            await self.handle_command(message, peer)
+        else:
+            print(f"{name}: {message}")
+
+    async def _on_enter(self, peer_id):
+        peer = self.peers[peer_id]
+        headers = peer[2]
+        print(headers)
+        for key in ["IK", "SPK", "OPK"]:
+            self.peer_keys[peer_id][key] = X25519PublicKey.from_public_bytes(
+                base64.b64decode(headers[key])
+            )
+        self.peer_keys[peer_id]["peer"] = peer_id
 
     async def handle_enter(self, cmds):
         peer = uuid.UUID(bytes=cmds.pop(0))
@@ -126,7 +194,9 @@ class ZRENode:
         headers = json.loads(cmds.pop(0).decode("utf-8"))
         logger.debug(headers)
         self.peers[peer] = [name, cmds, headers]
+        self.directory[name] = peer
         print(f"{name} {peer} entered")
+        await self._on_enter(peer)
 
     async def handle_join(self, cmds):
         peer = uuid.UUID(bytes=cmds.pop(0))
@@ -152,7 +222,6 @@ class ZRENode:
             self.groups[g].remove(peer)
         print(self.peers, self.groups)
 
-
     async def networkstream(self, queue):
         seq = 0
         while True:
@@ -173,7 +242,7 @@ async def readline(session):
     while True:
         with patch_stdout():
             line = await session.prompt_async()
-        yield (seq, bytes(line, 'utf-8'))
+        yield (seq, bytes(line, "utf-8"))
         seq += 1
 
 
@@ -190,6 +259,7 @@ async def async_main(pipe, node):
                 if seq != last_seq:
                     pipe.send(msg)
                     last_seq = seq
+
 
 def main():
     global logger
@@ -210,6 +280,7 @@ def main():
         exit_event.set()
 
     return 0
+
 
 if __name__ == "__main__":
     sys.exit(main())  # pragma: no cover
---
+++ zre_raft/zre_signal.py
@@ -0,0 +1,153 @@
+import base64
+
+from collections import defaultdict
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
+from cryptography.hazmat.primitives.asymmetric.ed25519 import (
+    Ed25519PublicKey,
+    Ed25519PrivateKey,
+)
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.backends import default_backend
+
+from Crypto.Cipher import AES
+
+
+def b64(msg):
+    # base64 encoding helper function
+    return base64.encodebytes(msg).decode("utf-8").strip()
+
+
+def b64k(public_key):
+    # base64 encoding helper function
+    key_bytes = public_key.public_bytes(
+        encoding=serialization.Encoding.Raw, format=serialization.PublicFormat.Raw
+    )
+    return base64.b64encode(key_bytes).decode("utf-8").strip()
+
+
+def hkdf(inp, length):
+    # use HKDF on an input to derive a key
+    hkdf = HKDF(
+        algorithm=hashes.SHA256(),
+        length=length,
+        salt=b"",
+        info=b"",
+        backend=default_backend(),
+    )
+    return hkdf.derive(inp)
+
+
+def pad(msg):
+    # pkcs7 padding
+    num = 16 - (len(msg) % 16)
+    return msg + bytes([num] * num)
+
+
+def unpad(msg):
+    # remove pkcs7 padding
+    return msg[: -msg[-1]]
+
+
+class SymmRatchet(object):
+    def __init__(self, key):
+        self.state = key
+
+    def next(self, inp=b""):
+        # turn the ratchet, changing the state and yielding a new key and IV
+        output = hkdf(self.state + inp, 80)
+        self.state = output[:32]
+        outkey, iv = output[32:64], output[64:]
+        return outkey, iv
+
+
+class SignalState:
+    def __init__(self):
+        self.IK = X25519PrivateKey.generate()
+        self.SPK = X25519PrivateKey.generate()
+        self.OPK = X25519PrivateKey.generate()
+        self.DHratchet = X25519PrivateKey.generate()
+        self.peer_keys = defaultdict(dict)
+        self.sessions = {}
+
+    def x3dh_receiver(self, other):
+        # perform the 4 Diffie Hellman exchanges (X3DH)
+        dh1 = self.SPK.exchange(other["IK"])
+        EK = other["EK"]
+        dh2 = self.IK.exchange(EK)
+        dh3 = self.SPK.exchange(EK)
+        dh4 = self.OPK.exchange(EK)
+        # print("receiver: ", EK.public_bytes(), dh1, dh2, dh3, dh4)
+        # the shared key is KDF(DH1||DH2||DH3||DH4)
+        self.sk = shared_key = hkdf(dh1 + dh2 + dh3 + dh4, 32)
+        self.sessions[other["peer"]] = shared_key
+        print(self.sessions)
+
+    def x3dh_sender(self, other):
+        # perform the 4 Diffie Hellman exchanges (X3DH)
+        dh1 = self.IK.exchange(other["SPK"])
+        dh2 = self.EK.exchange(other["IK"])
+        dh3 = self.EK.exchange(other["SPK"])
+        dh4 = self.EK.exchange(other["OPK"])
+        # print("sender: ", self.EK.public_key().public_bytes(), dh1, dh2, dh3, dh4)
+        # the shared key is KDF(DH1||DH2||DH3||DH4)
+        self.sk = shared_key = hkdf(dh1 + dh2 + dh3 + dh4, 32)
+        self.sessions[other["peer"]] = shared_key
+        print(self.sessions)
+
+    def establish_session(self, n, other, message):
+        self.EK = EK = X25519PrivateKey.generate()
+        self.peer_keys[other]["EK"] = EK.public_key()
+        message = " ".join(["/ephemeral", b64k(EK.public_key())])
+        n.whisper(other, message.encode("utf-8"))
+        self._on_message_send(other)
+
+    def _on_message_send(self, peer_id):
+        if peer_id in self.sessions:
+            return
+        self.x3dh_sender(self.peer_keys[peer_id])
+        self.init_ratchets()
+
+    def _on_message_receive(self, peer_id):
+        if peer_id in self.sessions:
+            return
+        self.x3dh_receiver(self.peer_keys[peer_id])
+        self.init_ratchets()
+
+    def init_ratchets(self):
+        # initialise the root chain with the shared key
+        self.root_ratchet = SymmRatchet(self.sk)
+        # initialise the sending and recving chains
+        self.recv_ratchet = SymmRatchet(self.root_ratchet.next()[0])
+        self.send_ratchet = SymmRatchet(self.root_ratchet.next()[0])
+
+    def dh_ratchet(self, other_public):
+        # perform a DH ratchet rotation using Other User's public key
+        dh_recv = self.DHratchet.exchange(other_public)
+        shared_recv = self.root_ratchet.next(dh_recv)[0]
+        # use Other User's public and our old private key
+        # to get a new recv ratchet
+        self.recv_ratchet = SymmRatchet(shared_recv)
+        print("Recv ratchet seed:", b64(shared_recv))
+        # generate a new key pair and send ratchet
+        # our new public key will be sent with the next message to Other User
+        self.DHratchet = X25519PrivateKey.generate()
+        dh_send = self.DHratchet.exchange(other_public)
+        shared_send = self.root_ratchet.next(dh_send)[0]
+        self.send_ratchet = SymmRatchet(shared_send)
+        print("Send ratchet seed:", b64(shared_send))
+
+    def send(self, other, msg):
+        key, iv = self.send_ratchet.next()
+        cipher = AES.new(key, AES.MODE_CBC, iv).encrypt(pad(msg))
+        print("Sending ciphertext to Other User:", b64(cipher))
+        # send ciphertext and current DH public key
+        other.recv(cipher, self.DHratchet.public_key())
+
+    def recv(self, cipher, other_public_key):
+        # receive Other User's new public key and use it to perform a DH
+        self.dh_ratchet(other_public_key)
+        key, iv = self.recv_ratchet.next()
+        # decrypt the message using the new recv ratchet
+        msg = unpad(AES.new(key, AES.MODE_CBC, iv).decrypt(cipher))
+        print("Decrypted message:", msg)
