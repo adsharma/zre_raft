55913573b6907c8611ed152e0b9ad5557e10394d
Arun Sharma <arun@sharma-home.net>
2021-01-04 10:30:30 -0800

    Fix setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,8 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open('README.md') as readme_file:
     readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
 
 requirements = ["aiostream", "zmq", "pyre", "prompt_toolkit"]
 
@@ -39,7 +36,7 @@ setup(
     },
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     include_package_data=True,
     keywords='zre_raft',
     name='zre_raft',
